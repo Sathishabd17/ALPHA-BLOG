1a374c60df8c6f81fd3718e1969c6b8dad554e65
Sathish kumar <sathishsekar444@gmail.com>
2020-11-28 18:20:37 +0530

    add create, edit, update actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,10 +11,24 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))
         if @article.save
             flash[:notice] = "Article has created successfully."
+            redirect_to @article
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article updated successfully."
             redirect_to @article
         else
             render 'new'
