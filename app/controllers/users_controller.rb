cfca45ac184289d1e674909a3aefd000ccbd2fbf
Sathish kumar <sathishsekar444@gmail.com>
2020-12-02 01:06:01 +0530

    Add edit page and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,26 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [ :edit, :update]
+
     def new
         @user = User.new
+    end
+
+    def edit
+    end
+
+    def update
+        if @user.update(user_params)
+            flash[:notice] = "Your account information was updated successfully"
+            redirect_to articles_path
+        else
+            render 'edit'
+        end
     end
 
     def create
         @user = User.new(user_params)
         if @user.save
-            flash[:notice] = "Welcome to Alpha Blog #{@user.username}, you have successfully signed up"
+            flash[:notice] = "Welcome to Alpha Blog #{@user.username}, you have successfully signed in"
             redirect_to articles_path
         else
             render 'new'
@@ -14,6 +28,10 @@ class UsersController < ApplicationController
     end
 
     private
+    def set_user
+        @user = User.find(params[:id])
+    end
+
     def user_params
         params.require(:user).permit(:username, :email, :password)
     end
