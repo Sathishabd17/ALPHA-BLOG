4f5eada231b90235b1c22bd54df7ebd4f412db64
Sathish kumar <sathishsekar444@gmail.com>
2020-12-02 23:18:20 +0530

    add links to layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,6 @@ module ApplicationHelper
         hash = Digest::MD5.hexdigest(email_address)
         size = options[:size]
         image_src = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
-        image_tag(image_src, alt: user.username, class:"rounded mx-auto d-block")
+        image_tag(image_src, alt: user.username, class:"rounded shadow mx-auto d-block")
     end
 end
