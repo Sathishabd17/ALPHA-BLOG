5b45869ed9e28312252dbd3f71fcc48d5aab17d6
Sathish kumar <sathishsekar444@gmail.com>
2020-12-04 00:48:18 +0530

    Add authentication helper method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             flash[:notice] = "Welcome to Alpha Blog #{@user.username}, you have successfully signed in"
             redirect_to @user
         else
