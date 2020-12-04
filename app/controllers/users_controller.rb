62bc5ce7ef3de7b7bd3447773a8d63ddb95e222d
Sathish kumar <sathishsekar444@gmail.com>
2020-12-04 11:50:27 +0530

    Require user and same user restrictions for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:show, :edit, :update]
+    before_action :require_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update]
 
     def show
         @articles = @user.articles.paginate(page: params[:page], per_page: 5)
@@ -43,5 +45,12 @@ class UsersController < ApplicationController
 
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+
+    def require_same_user
+        if @user != current_user
+            flash[:alert] = "You can only edit your own profile"
+            redirect_to @user
+        end
     end
 end
