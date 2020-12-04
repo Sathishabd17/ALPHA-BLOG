cc23212cd607ff3dc82bd87b07b9d5bbf9c6db78
Sathish kumar <sathishsekar444@gmail.com>
2020-12-04 11:36:24 +0530

    Require user and same user restrictions for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
     before_action :set_article, only: [:show, :edit, :update, :destroy]
-
+    before_action :require_user, except: [:show, :index]
+    before_action :require_same_user, only: [:edit, :update, :delete]
 
     def show
     end
@@ -48,5 +49,12 @@ class ArticlesController < ApplicationController
 
     def article_params
         params.require(:article).permit(:title, :description)
+    end
+
+    def require_same_user
+        if @article.user != current_user
+            flash[:alert] = "You can only edit or delete your own articles"
+            redirect_to @article
+        end
     end
 end
