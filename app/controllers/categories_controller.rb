faffbd9f6cc7564f2d6c65bfb52d366cfb4a3d0c
Sathish kumar <sathishsekar444@gmail.com>
2020-12-06 00:12:24 +0530

    Add unit test and functional test for category and category controller

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,28 @@
+class CategoriesController < ApplicationController
+    def show
+
+    end
+
+    def new
+        @category = Category.new
+    end
+
+    def create
+        @category = Category.new(category_params)
+        if @category.save
+            flash[:notice] = "Category was created successfully"
+            redirect_to @category
+        else
+            render 'new'
+        end
+    end
+
+    def index
+
+    end
+
+    private
+    def category_params
+        params.require(:category).permit(:name)
+    end
+end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,8 +1,33 @@
 require 'test_helper.rb'
 
 class CategoryTest < ActiveSupport::TestCase
+    def setup
+        @category = Category.new(name: "Sports")
+    end
+
     test "category should be valid" do
-        @category = Category.new
         assert @category.valid?, "category is not valid"
     end
+
+    test "name should be present" do
+        @category.name = " "
+        assert_not @category.valid?, "name is not present"
+    end
+
+    test "name should be unique" do
+        @category.save
+        @category2 = Category.new(name: "Sports")
+        assert_not @category2.valid?, "already this name taken"
+    end
+
+    test "name should not be too long" do
+        @category.name = "a" * 30
+        assert_not @category.valid?, "name is too long"   
+    end
+
+    test "name should not be too short" do
+        @category.name = "aa"
+        assert_not @category.valid?, "name is too short"
+    end
+
 end
