7bc130a44877d6158e9359f0e5d6be6e7d4e31b9
Sathish kumar <sathishsekar444@gmail.com>
2020-12-06 14:34:36 +0530

    Create article_category table and make has_many association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_05_084848) do
+ActiveRecord::Schema.define(version: 2020_12_06_080030) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -21,12 +26,6 @@ ActiveRecord::Schema.define(version: 2020_12_05_084848) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "categories_tables", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
